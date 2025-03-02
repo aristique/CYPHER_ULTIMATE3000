4361c9068d0b696780d870a560539e1636d1dc17
Aristique <arissmoonest@gmail.com>
2025-03-02 20:51:03 +0200

    Запретил изменять размер окна приложения

--- CYPHER_ULTIMATE3000/Form1.Designer.cs
+++ CYPHER_ULTIMATE3000/Form1.Designer.cs
@@ -1,15 +1,11 @@
-﻿namespace CYPHER_ULTIMATE3000
+﻿using System.Windows.Forms;
+
+namespace CYPHER_ULTIMATE3000
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
@@ -22,21 +18,21 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+
         private void InitializeComponent()
         {
             this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 720);
-            this.Name = "Form1";
-            this.Text = "Form1";
+        
+            // Отключил изменение размера окна приложения
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Cypher3000";
+            this.Text = "Cypher3000";
             this.ResumeLayout(false);
 
         }
