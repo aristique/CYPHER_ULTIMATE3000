a0fc8417e7b7188ba1ca5a7969f703b805db74b9
Aristique <arissmoonest@gmail.com>
2025-03-12 20:13:06 +0200

    Добавил шифрование RSA (Дешифрование пока не сделал)

--- CYPHER_ULTIMATE3000/Form1.Designer.cs
+++ CYPHER_ULTIMATE3000/Form1.Designer.cs
@@ -27,16 +27,18 @@ namespace CYPHER_ULTIMATE3000
             this.InputTextBox = new System.Windows.Forms.TextBox();
             this.InputText = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.KeyTextBox = new System.Windows.Forms.TextBox();
-            this.KeyText = new System.Windows.Forms.Label();
-            this.GenerateKeyButton = new System.Windows.Forms.Button();
+            this.PrivKeyTextBox = new System.Windows.Forms.TextBox();
+            this.PrivKeyText = new System.Windows.Forms.Label();
+            this.GeneratePrivKeyButton = new System.Windows.Forms.Button();
+            this.PubKeyText = new System.Windows.Forms.Label();
+            this.PubKeyTextBox = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.EncryptButton = new System.Windows.Forms.Button();
             this.DecryptButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.ChooseAlgText = new System.Windows.Forms.Label();
-            this.SymAlgRadioButton = new System.Windows.Forms.RadioButton();
-            this.AsymAlgRadioButton = new System.Windows.Forms.RadioButton();
+            this.AESRadioButton = new System.Windows.Forms.RadioButton();
+            this.RSARadioButton = new System.Windows.Forms.RadioButton();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -66,7 +68,7 @@ namespace CYPHER_ULTIMATE3000
             // 
             this.OutputText.AutoSize = true;
             this.OutputText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.OutputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.OutputText.Location = new System.Drawing.Point(686, 0);
             this.OutputText.Name = "OutputText";
             this.OutputText.Size = new System.Drawing.Size(653, 20);
@@ -98,7 +100,7 @@ namespace CYPHER_ULTIMATE3000
             // 
             this.InputText.AutoSize = true;
             this.InputText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.InputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.InputText.Location = new System.Drawing.Point(3, 0);
             this.InputText.Name = "InputText";
             this.InputText.Size = new System.Drawing.Size(652, 20);
@@ -110,51 +112,77 @@ namespace CYPHER_ULTIMATE3000
             // 
             this.tableLayoutPanel2.ColumnCount = 3;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel2.Controls.Add(this.KeyTextBox, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.KeyText, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.GenerateKeyButton, 1, 1);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.61832F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.45802F));
+            this.tableLayoutPanel2.Controls.Add(this.PrivKeyTextBox, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.PrivKeyText, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.GeneratePrivKeyButton, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.PubKeyText, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.PubKeyTextBox, 0, 3);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 490);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(655, 81);
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(655, 181);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
-            // KeyTextBox
+            // PrivKeyTextBox
             // 
-            this.KeyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.KeyTextBox.Location = new System.Drawing.Point(3, 23);
-            this.KeyTextBox.Multiline = true;
-            this.KeyTextBox.Name = "KeyTextBox";
-            this.KeyTextBox.Size = new System.Drawing.Size(321, 55);
-            this.KeyTextBox.TabIndex = 0;
+            this.PrivKeyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PrivKeyTextBox.Location = new System.Drawing.Point(3, 21);
+            this.PrivKeyTextBox.Multiline = true;
+            this.PrivKeyTextBox.Name = "PrivKeyTextBox";
+            this.PrivKeyTextBox.Size = new System.Drawing.Size(321, 66);
+            this.PrivKeyTextBox.TabIndex = 0;
             // 
-            // KeyText
+            // PrivKeyText
             // 
-            this.KeyText.AutoSize = true;
-            this.KeyText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.KeyText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.KeyText.Location = new System.Drawing.Point(3, 0);
-            this.KeyText.Name = "KeyText";
-            this.KeyText.Size = new System.Drawing.Size(321, 20);
-            this.KeyText.TabIndex = 1;
-            this.KeyText.Text = "Ключ:";
-            this.KeyText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PrivKeyText.AutoSize = true;
+            this.PrivKeyText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PrivKeyText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PrivKeyText.Location = new System.Drawing.Point(3, 0);
+            this.PrivKeyText.Name = "PrivKeyText";
+            this.PrivKeyText.Size = new System.Drawing.Size(321, 18);
+            this.PrivKeyText.TabIndex = 1;
+            this.PrivKeyText.Text = "Приватный ключ:";
+            this.PrivKeyText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // GenerateKeyButton
+            // GeneratePrivKeyButton
             // 
-            this.GenerateKeyButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.GenerateKeyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.GenerateKeyButton.Location = new System.Drawing.Point(330, 26);
-            this.GenerateKeyButton.Name = "GenerateKeyButton";
-            this.GenerateKeyButton.Size = new System.Drawing.Size(157, 49);
-            this.GenerateKeyButton.TabIndex = 2;
-            this.GenerateKeyButton.Text = "Сгенерировать ключ";
-            this.GenerateKeyButton.UseVisualStyleBackColor = true;
-            this.GenerateKeyButton.Click += new System.EventHandler(this.GenerateKeyButton_Click);
+            this.GeneratePrivKeyButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.GeneratePrivKeyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GeneratePrivKeyButton.Location = new System.Drawing.Point(330, 21);
+            this.GeneratePrivKeyButton.Name = "GeneratePrivKeyButton";
+            this.GeneratePrivKeyButton.Size = new System.Drawing.Size(187, 66);
+            this.GeneratePrivKeyButton.TabIndex = 2;
+            this.GeneratePrivKeyButton.Text = "Сгенерировать ключ";
+            this.GeneratePrivKeyButton.UseVisualStyleBackColor = true;
+            this.GeneratePrivKeyButton.Click += new System.EventHandler(this.GenerateAESKeyButton_Click);
+            // 
+            // PubKeyText
+            // 
+            this.PubKeyText.AutoSize = true;
+            this.PubKeyText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.PubKeyText.Location = new System.Drawing.Point(3, 90);
+            this.PubKeyText.Name = "PubKeyText";
+            this.PubKeyText.Size = new System.Drawing.Size(136, 16);
+            this.PubKeyText.TabIndex = 3;
+            this.PubKeyText.Text = "Публичный ключ:";
+            this.PubKeyText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // PubKeyTextBox
+            // 
+            this.PubKeyTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PubKeyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PubKeyTextBox.Location = new System.Drawing.Point(3, 111);
+            this.PubKeyTextBox.Multiline = true;
+            this.PubKeyTextBox.Name = "PubKeyTextBox";
+            this.PubKeyTextBox.ReadOnly = true;
+            this.PubKeyTextBox.Size = new System.Drawing.Size(321, 67);
+            this.PubKeyTextBox.TabIndex = 4;
             // 
             // tableLayoutPanel3
             // 
@@ -182,7 +210,7 @@ namespace CYPHER_ULTIMATE3000
             this.EncryptButton.TabIndex = 0;
             this.EncryptButton.Text = "Шифровать текст";
             this.EncryptButton.UseVisualStyleBackColor = true;
-            this.EncryptButton.Click += new System.EventHandler(this.EncyptButton_Click);
+            this.EncryptButton.Click += new System.EventHandler(this.AESEncyptButton_Click);
             // 
             // DecryptButton
             // 
@@ -194,14 +222,14 @@ namespace CYPHER_ULTIMATE3000
             this.DecryptButton.TabIndex = 1;
             this.DecryptButton.Text = "Дешифровать текст";
             this.DecryptButton.UseVisualStyleBackColor = true;
-            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            this.DecryptButton.Click += new System.EventHandler(this.AESDecryptButton_Click);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.ChooseAlgText);
-            this.panel1.Controls.Add(this.SymAlgRadioButton);
-            this.panel1.Controls.Add(this.AsymAlgRadioButton);
-            this.panel1.Location = new System.Drawing.Point(12, 574);
+            this.panel1.Controls.Add(this.AESRadioButton);
+            this.panel1.Controls.Add(this.RSARadioButton);
+            this.panel1.Location = new System.Drawing.Point(12, 677);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 79);
             this.panel1.TabIndex = 3;
@@ -215,27 +243,29 @@ namespace CYPHER_ULTIMATE3000
             this.ChooseAlgText.TabIndex = 2;
             this.ChooseAlgText.Text = "Выбор алгоритма";
             // 
-            // SymAlgRadioButton
+            // AESRadioButton
             // 
-            this.SymAlgRadioButton.AutoSize = true;
-            this.SymAlgRadioButton.Location = new System.Drawing.Point(3, 36);
-            this.SymAlgRadioButton.Name = "SymAlgRadioButton";
-            this.SymAlgRadioButton.Size = new System.Drawing.Size(183, 17);
-            this.SymAlgRadioButton.TabIndex = 1;
-            this.SymAlgRadioButton.TabStop = true;
-            this.SymAlgRadioButton.Text = "Симметричный алгоритм (AES)";
-            this.SymAlgRadioButton.UseVisualStyleBackColor = true;
+            this.AESRadioButton.AutoSize = true;
+            this.AESRadioButton.Location = new System.Drawing.Point(3, 36);
+            this.AESRadioButton.Name = "AESRadioButton";
+            this.AESRadioButton.Size = new System.Drawing.Size(183, 17);
+            this.AESRadioButton.TabIndex = 1;
+            this.AESRadioButton.TabStop = true;
+            this.AESRadioButton.Text = "Симметричный алгоритм (AES)";
+            this.AESRadioButton.UseVisualStyleBackColor = true;
+            this.AESRadioButton.CheckedChanged += new System.EventHandler(this.AESRadioButton_CheckedChanged);
             // 
-            // AsymAlgRadioButton
+            // RSARadioButton
             // 
-            this.AsymAlgRadioButton.AutoSize = true;
-            this.AsymAlgRadioButton.Location = new System.Drawing.Point(3, 59);
-            this.AsymAlgRadioButton.Name = "AsymAlgRadioButton";
-            this.AsymAlgRadioButton.Size = new System.Drawing.Size(190, 17);
-            this.AsymAlgRadioButton.TabIndex = 0;
-            this.AsymAlgRadioButton.TabStop = true;
-            this.AsymAlgRadioButton.Text = "Асимметричный алгоритм (RSA)";
-            this.AsymAlgRadioButton.UseVisualStyleBackColor = true;
+            this.RSARadioButton.AutoSize = true;
+            this.RSARadioButton.Location = new System.Drawing.Point(3, 59);
+            this.RSARadioButton.Name = "RSARadioButton";
+            this.RSARadioButton.Size = new System.Drawing.Size(190, 17);
+            this.RSARadioButton.TabIndex = 0;
+            this.RSARadioButton.TabStop = true;
+            this.RSARadioButton.Text = "Асимметричный алгоритм (RSA)";
+            this.RSARadioButton.UseVisualStyleBackColor = true;
+            this.RSARadioButton.CheckedChanged += new System.EventHandler(this.RSARadioButton_CheckedChanged);
             // 
             // Form1
             // 
@@ -270,16 +300,18 @@ namespace CYPHER_ULTIMATE3000
         private Label InputText;
         private Label OutputText;
         private TableLayoutPanel tableLayoutPanel2;
-        private TextBox KeyTextBox;
-        private Label KeyText;
-        private Button GenerateKeyButton;
+        private TextBox PrivKeyTextBox;
+        private Label PrivKeyText;
+        private Button GeneratePrivKeyButton;
         private TableLayoutPanel tableLayoutPanel3;
         private Button EncryptButton;
         private Button DecryptButton;
         private Panel panel1;
-        private RadioButton SymAlgRadioButton;
-        private RadioButton AsymAlgRadioButton;
+        private RadioButton AESRadioButton;
+        private RadioButton RSARadioButton;
         private Label ChooseAlgText;
+        private Label PubKeyText;
+        private TextBox PubKeyTextBox;
     }
 }
 
--- CYPHER_ULTIMATE3000/Form1.cs
+++ CYPHER_ULTIMATE3000/Form1.cs
@@ -11,14 +11,18 @@ namespace CYPHER_ULTIMATE3000
         public Form1()
         {
             InitializeComponent();
+
+            AESRadioButton.Checked = true;
+            PubKeyText.Visible = false;
+            PubKeyTextBox.Visible = false;
         }
+
         // ШИФРОВАНИЕ AES
         //
         //
 
-        public static string Encrypt(string plainText, string password)
+        public static string AESEncrypt(string plainText, string password)
         {
-            byte[] cipherData;
             using (Aes aes = Aes.Create())
             {
                 byte[] salt = new byte[16];
@@ -55,7 +59,7 @@ namespace CYPHER_ULTIMATE3000
             }
         }
 
-        public static string Decrypt(string combinedString, string password)
+        public static string AESDecrypt(string combinedString, string password)
         {
             try
             {
@@ -100,30 +104,64 @@ namespace CYPHER_ULTIMATE3000
         //
         // ШИФРОВАНИЕ AES
 
-        private void EncyptButton_Click(object sender, EventArgs e)
+        // ШИФРОВАНИЕ RSA
+        //
+        //
+
+        public static byte[] RSAEncrypt(string dataToEncrypt, string publicKey)
+        {
+            //using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.FromXmlString(publicKey);
+                byte[] messageBytes = Encoding.UTF8.GetBytes(dataToEncrypt);
+                byte[] encryptedBytes = rsa.Encrypt(messageBytes, RSAEncryptionPadding.Pkcs1);
+                return encryptedBytes;
+            }
+        }
+
+        public static string RSADecrypt(string privateKey, byte[] encryptedBytes)
+        {
+            //using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.FromXmlString(privateKey);
+                byte[] decryptedBytes = rsa.Decrypt(encryptedBytes, RSAEncryptionPadding.Pkcs1);
+                string decryptedMessage = Encoding.UTF8.GetString(decryptedBytes);
+                return decryptedMessage;
+            }
+        }
+
+        //
+        //
+        // ШИФРОВАНИЕ RSA
+
+        // Кнопка AES шифрования
+        private void AESEncyptButton_Click(object sender, EventArgs e)
         {
             if (InputTextBox.Text != "")
             {
-                if (KeyTextBox.Text != "")
+                if (PrivKeyTextBox.Text != "")
                 {
-                    string keyCrypt = KeyTextBox.Text;
+                    string keyCrypt = PrivKeyTextBox.Text;
                     OutputTextBox.Clear();
-                    OutputTextBox.AppendText(Encrypt(InputTextBox.Text, keyCrypt));
+                    OutputTextBox.AppendText(AESEncrypt(InputTextBox.Text, keyCrypt));
                 }
                 else MessageBox.Show("Введите ключ.", "Ошибка!");
             }
             else MessageBox.Show("Введите текст.", "Ошибка!");
         }
-
-        private void DecryptButton_Click(object sender, EventArgs e)
+        
+        // Кнопка AES дешифрования
+        private void AESDecryptButton_Click(object sender, EventArgs e)
         {
             if (InputTextBox.Text != "")
             {
-                if (KeyTextBox.Text != "")
+                if (PrivKeyTextBox.Text != "")
                 {
-                    string keyCrypt = KeyTextBox.Text;
+                    string keyCrypt = PrivKeyTextBox.Text;
                     OutputTextBox.Clear();
-                    string decryptedText = Decrypt(InputTextBox.Text, keyCrypt);
+                    string decryptedText = AESDecrypt(InputTextBox.Text, keyCrypt);
                     if (decryptedText.StartsWith("Ошибка"))
                     {
                         MessageBox.Show(decryptedText, "Ошибка!");
@@ -138,17 +176,110 @@ namespace CYPHER_ULTIMATE3000
             else MessageBox.Show("Введите текст.", "Ошибка!");
         }
 
-        private void GenerateKeyButton_Click(object sender, EventArgs e)
+        // Кнопка генерации приватного ключа AES
+        private void GenerateAESKeyButton_Click(object sender, EventArgs e)
         {
             Random random = new Random();
             StringBuilder result = new StringBuilder(16);
-            KeyTextBox.Clear();
+            PrivKeyTextBox.Clear();
             for (int i = 0; i < 16; i++)
             {
                 char randomChar = (char)random.Next(33, 127);
                 result.Append(randomChar);
             }
-            KeyTextBox.AppendText(result.ToString());
+            PrivKeyTextBox.AppendText(result.ToString());
+        }
+
+        // Кнопка RSA шифрования
+        private void RSAEncyptButton_Click(object sender, EventArgs e)
+        {
+            if (InputTextBox.Text != "")
+            {
+                if (PubKeyTextBox.Text != "")
+                {
+                    string keyCrypt = PubKeyTextBox.Text;
+                    OutputTextBox.Clear();
+                    OutputTextBox.AppendText(Convert.ToBase64String(RSAEncrypt(InputTextBox.Text, keyCrypt)));
+                }
+                else MessageBox.Show("Введите ключ.", "Ошибка!");
+            }
+            else MessageBox.Show("Введите текст.", "Ошибка!");
+        }
+
+        // Кнопка RSA дешифрования
+        private void RSADecryptButton_Click(object sender, EventArgs e)
+        {
+            /*if (InputTextBox.Text != "")
+            {
+                if (PrivKeyTextBox.Text != "")
+                {
+                    string keyCrypt = PrivKeyTextBox.Text;
+                    OutputTextBox.Clear();
+                    string decryptedText = RSADecrypt(InputTextBox.Text, Convert.FromBase64String(keyCrypt));
+                    if (decryptedText.StartsWith("Ошибка"))
+                    {
+                        MessageBox.Show(decryptedText, "Ошибка!");
+                    }
+                    else
+                    {
+                        OutputTextBox.AppendText(decryptedText);
+                    }
+                }
+                else MessageBox.Show("Введите ключ.", "Ошибка!");
+            }
+            else MessageBox.Show("Введите текст.", "Ошибка!");*/
+        }
+
+        // Кнопка генерации приватного ключа RSA
+        private void GenerateRSAKeyButton_Click(Object sender, EventArgs e)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                 PubKeyTextBox.Text = rsa.ToXmlString(false);
+                 PrivKeyTextBox.Text = rsa.ToXmlString(true);
+            }
+        }
+
+        private void AESRadioButton_CheckedChanged(Object sender, EventArgs e)
+        {
+            if (AESRadioButton.Checked)
+            {
+                PubKeyText.Visible = false;
+                PubKeyTextBox.Visible = false;
+
+                GeneratePrivKeyButton.Click -= GenerateAESKeyButton_Click;
+                GeneratePrivKeyButton.Click -= GenerateRSAKeyButton_Click;
+                GeneratePrivKeyButton.Click += GenerateAESKeyButton_Click;
+
+                EncryptButton.Click -= AESEncyptButton_Click;
+                EncryptButton.Click -= RSAEncyptButton_Click;
+                EncryptButton.Click += AESEncyptButton_Click;
+
+                DecryptButton.Click -= AESDecryptButton_Click;
+                DecryptButton.Click -= RSADecryptButton_Click;
+                DecryptButton.Click += AESDecryptButton_Click;
+            }
+        }
+
+        private void RSARadioButton_CheckedChanged(Object sender, EventArgs e)
+        {
+            if (RSARadioButton.Checked)
+            {
+                PubKeyText.Visible = true;
+                PubKeyTextBox.Visible = true;
+
+                GeneratePrivKeyButton.Click -= GenerateAESKeyButton_Click;
+                GeneratePrivKeyButton.Click -= GenerateRSAKeyButton_Click;
+                GeneratePrivKeyButton.Click += GenerateRSAKeyButton_Click;
+
+                EncryptButton.Click -= AESEncyptButton_Click;
+                EncryptButton.Click -= RSAEncyptButton_Click;
+                EncryptButton.Click += RSAEncyptButton_Click;
+
+                DecryptButton.Click -= AESDecryptButton_Click;
+                DecryptButton.Click -= RSADecryptButton_Click;
+                DecryptButton.Click += RSADecryptButton_Click;
+            }
         }
     }
 }
